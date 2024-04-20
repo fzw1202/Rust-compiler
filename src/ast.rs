ac37d6b109a25d48009b5922301de042ace00592
fzw <fzw1202@gmail.com>
2024-04-20 21:14:45 +0800

    lv3 - expression

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,7 @@
 use std::io::{self};
 
+static mut CNT: i32 = 0;
+
 pub struct CompUnit {
     pub func_def: FuncDef,
 }
@@ -60,24 +62,19 @@ pub struct Stmt {
 
 impl Stmt {
     fn dump(&self, s: &mut String) -> io::Result<()> {
-        let cnt: i32 = self.exp.dump(s);
-        if cnt >= 0 {
-            s.push_str(&format!("  ret %{}\n", cnt));
-        }
-        else {
-            s.push_str(&format!("  ret {}\n", -cnt))
-        }
+        let a = self.exp.dump(s);
+        s.push_str(&format!("  ret %{}\n", a));
         Ok(())
     }
 }
 
 pub struct Exp {
-    pub uexp: UnaryExp,
+    pub loexp: LOrExp,
 }
 
 impl Exp {
     fn dump(&self, s: &mut String) -> i32 {
-        self.uexp.dump(s)
+        self.loexp.dump(s)
     }
 }
 
@@ -90,7 +87,13 @@ impl PrimaryExp {
     fn dump(&self, s: &mut String) -> i32 {
         match self {
             PrimaryExp::Exp(exp) => exp.dump(s),
-            PrimaryExp::Number(ref num) => -num - 1,
+            PrimaryExp::Number(ref num) => {
+                unsafe {
+                    CNT += 1;
+                    s.push_str(&format!("  %{} = sub {}, 0\n", CNT, num));
+                    return CNT;
+                };
+            },
         }
     }
 }
@@ -105,21 +108,21 @@ impl UnaryExp {
         match self {
             UnaryExp::Primary(pexp) => pexp.dump(s),
             UnaryExp::Unary(uop, uexp) => {
-                let x = uexp.dump(s);
-                if x < 0 {
+                let a = uexp.dump(s);
+                unsafe {
                     match uop {
-                        UnaryOp::Pos => return x + 1,
-                        UnaryOp::Neg => s.push_str(&format!("  %{} = sub 0, {}\n", 0, -x - 1)),
-                        UnaryOp::Not => s.push_str(&format!("  %{} = eq {}, 0\n", 0, -x - 1)),
+                        UnaryOp::Pos => return a,
+                        UnaryOp::Neg => {
+                            CNT += 1;
+                            s.push_str(&format!("  %{} = sub 0, %{}\n", CNT, a));
+                            return CNT;
+                        },
+                        UnaryOp::Not => {
+                            CNT += 1;
+                            s.push_str(&format!("  %{} = eq %{}, 0\n", CNT, a));
+                            return CNT;
+                        },
                     };
-                    return 0;
-                } else {
-                    match uop {
-                        UnaryOp::Pos => return x,
-                        UnaryOp::Neg => s.push_str(&format!("  %{} = sub 0, %{}\n", x + 1, x)),
-                        UnaryOp::Not => s.push_str(&format!("  %{} = eq %{}, 0\n", x + 1, x)),
-                    };
-                    return x + 1;
                 }
             }
         }
@@ -130,4 +133,179 @@ pub enum UnaryOp {
     Pos,
     Neg,
     Not,
+}
+
+pub enum MulOp {
+    Mul,
+    Div,
+    Mod,
+}
+
+pub enum AddOp {
+    Add,
+    Sub,
+}
+
+pub enum MulExp {
+    Unary(UnaryExp),
+    Mul(Box<MulExp>, MulOp, UnaryExp),
+}
+
+impl MulExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            MulExp::Unary(uexp) => uexp.dump(s),
+            MulExp::Mul(mexp, mop, uexp) => {
+                let a: i32 = mexp.dump(s);
+                let b: i32 = uexp.dump(s);
+                unsafe {
+                    CNT += 1;
+                    match mop {
+                        MulOp::Div => s.push_str(&format!("  %{} = div %{}, %{}", CNT, a, b)),
+                        MulOp::Mod => s.push_str(&format!("  %{} = mod %{}, %{}", CNT, a, b)),
+                        MulOp::Mul => s.push_str(&format!("  %{} = mul %{}, %{}", CNT, a, b)),
+                    };
+                    return CNT;
+                }
+            }
+        }
+    }
+}
+
+pub enum AddExp {
+    Mul(MulExp),
+    Add(Box<AddExp>, AddOp, MulExp),
+}
+
+impl AddExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            AddExp::Mul(mexp) => mexp.dump(s),
+            AddExp::Add(aexp, aop, mexp) => {
+                let a: i32 = aexp.dump(s);
+                let b: i32 = mexp.dump(s);
+                unsafe { 
+                    CNT += 1; 
+                    match aop {
+                        AddOp::Add => s.push_str(&format!("  %{} = add %{}, %{}", CNT, a, b)),
+                        AddOp::Sub => s.push_str(&format!("  %{} = sub %{}, %{}", CNT, a, b)),
+                    };
+                    return CNT;
+                }
+            },
+        }
+    }
+}
+
+pub enum RelOp {
+    Less,
+    Greater,
+    LessEq,
+    GreaterEq,
+}
+
+pub enum RelExp {
+    Add(AddExp),
+    Rel(Box<RelExp>, RelOp, AddExp),
+}
+
+impl RelExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            RelExp::Add(aexp) => aexp.dump(s),
+            RelExp::Rel(rexp, rop, aexp) => {
+                let a: i32 = rexp.dump(s);
+                let b: i32 = aexp.dump(s);
+                unsafe {
+                    CNT += 1;
+                    match rop {
+                        RelOp::Less => s.push_str(&format!("  %{} = lt %{}, %{}", CNT, a, b)),
+                        RelOp::Greater => s.push_str(&format!("  %{} = gt %{}, %{}", CNT, a, b)),
+                        RelOp::LessEq => s.push_str(&format!("  %{} = le %{}, %{}", CNT, a, b)),
+                        RelOp::GreaterEq => s.push_str(&format!("  %{} = ge %{}, %{}", CNT, a, b)),
+                    };
+                    return CNT;
+                }
+            },
+        }
+    }
+}
+
+pub enum EqOp {
+    Eq,
+    Neq,
+}
+
+pub enum EqExp {
+    Rel(RelExp),
+    Eq(Box<EqExp>, EqOp, RelExp),
+}
+
+impl EqExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            EqExp::Rel(rexp) => rexp.dump(s),
+            EqExp::Eq(eexp, eop, rexp) => {
+                let a: i32 = eexp.dump(s);
+                let b: i32 = rexp.dump(s);
+                unsafe {
+                    CNT += 1;
+                    match eop {
+                        EqOp::Eq => s.push_str(&format!("  %{} = eq %{}, %{}", CNT, a, b)),
+                        EqOp::Neq => s.push_str(&format!("  %{} = ne %{}, %{}", CNT, a, b)),
+                    }
+                    return CNT;
+                }
+            },
+        }
+    }
+}
+
+pub enum LAndExp {
+    Eq(EqExp),
+    LAnd(Box<LAndExp>, EqExp),
+}
+
+impl LAndExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            LAndExp::Eq(eexp) => eexp.dump(s),
+            LAndExp::LAnd(laexp, eexp) => {
+                let a: i32 = laexp.dump(s);
+                let b: i32 = eexp.dump(s);
+                unsafe {
+                    s.push_str(&format!("  %{} = eq %{}, 0", CNT + 1, a));
+                    s.push_str(&format!("  %{} = eq %{}, 0", CNT + 2, b));
+                    s.push_str(&format!("  %{} = or %{}, %{}", CNT + 3, CNT + 1, CNT + 2));
+                    s.push_str(&format!("  %{} = eq 0, %{}", CNT + 4, CNT + 3));
+                    CNT += 4;
+                    return CNT;
+                }
+            },
+        }
+    }
+}
+
+pub enum LOrExp {
+    LAnd(LAndExp),
+    LOr(Box<LOrExp>, LAndExp),
+}
+
+impl LOrExp {
+    fn dump(&self, s: &mut String) -> i32 {
+        match self {
+            LOrExp::LAnd(laexp) => laexp.dump(s),
+            LOrExp::LOr(loexp, laexp) => {
+                let a: i32 = loexp.dump(s);
+                let b: i32 = laexp.dump(s);
+                unsafe {
+                    s.push_str(&format!("  %{} = ne %{}, 0", CNT + 1, a));
+                    s.push_str(&format!("  %{} = ne %{}, 0", CNT + 2, b));
+                    s.push_str(&format!("  %{} = or %{}, %{}", CNT + 3, CNT + 1, CNT + 2));
+                    CNT += 3;
+                    return CNT;
+                }
+            },
+        }
+    }
 }
